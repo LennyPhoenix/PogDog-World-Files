94eb14843abe4874beed2b7887fe4a5e881b01af
LennyPhoenix <LennyPhoenixC@gmail.com>
2021-06-01 12:38:40 +0100

    ✅ Add survival world

---
+++ survival/start.cmd
@@ -0,0 +1,1 @@
+java -Xmx3500M -Xms3500M -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -XX:+AlwaysPreTouch -XX:+UseLargePagesInMetaspace -jar server.jar
